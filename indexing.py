06948106794f3864fc70dd776d7062df04289563
Narayan Tamatta <narayantamatta@yahoo.com>
2025-11-19 16:52:58 +0530

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -5,4 +5,4 @@ str1="Hello World"
 print('value at (3)', str1[3])
 print('value at (-3)', str1[-3])
 print('value at (0)', str1[0])
-print('value at (5)', str1[5])
+print('value at (5)', str1[5])  #print blank space
