40794f2456c7c622731b3e0920c7100094f7e5cb
Narayan Tamatta <narayantamatta@yahoo.com>
2025-11-19 17:49:41 +0530

    Revised if statement in python

---
+++ if.py
@@ -0,0 +1,15 @@
+age= 20
+if age >19:
+    print("Your eligible to vote")
+
+print("rest of the code")
+
+age= int(input("Enter your age:"))
+if age >25:
+    print("Your eligible to vote")
+if age<=18:
+    print("Sorry you stii underaged")
+if age>=20:
+    print("Please Renew your license at earliest as possible")
+
+print("Thank you for showing interest in National voter list")
