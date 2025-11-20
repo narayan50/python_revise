d19727f3e63bf913f0bdd1adc50cba383dd6aacd
Narayan Tamatta <narayantamatta@yahoo.com>
2025-11-20 16:49:43 +0530

    ok

--- if.py
+++ if.py
@@ -1,15 +1,22 @@
-age= 20
-if age >19:
-    print("Your eligible to vote")
+# age= 20
+# if age >19:
+#     print("Your eligible to vote")56
 
-print("rest of the code")
 
-age= int(input("Enter your age:"))
-if age >25:
-    print("Your eligible to vote")
-if age<=18:
-    print("Sorry you stii underaged")
-if age>=20:
-    print("Please Renew your license at earliest as possible")
+# print("rest of the code")
 
-print("Thank you for showing interest in National voter list")
+# age= int(input("Enter your age:"))
+# if age >25:
+#     print("Your eligible to vote")
+# if age<=18:
+#     print("Sorry you stii underaged")
+# if age>=20:
+#     print("Please Renew your license at earliest as possible")
+
+# print("Thank you for showing interest in National voter list")
+
+
+n=5
+i=1
+while i>=70:
+  print(i*n , end= " ")
